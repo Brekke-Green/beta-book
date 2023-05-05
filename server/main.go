c0c73623d6acd5d5e0f1d4d8f01726a819e326e3
Brekke-Green <brekke.andrew.green@gmail.com>
2023-05-05 15:05:20 -0400

    Update routes

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
         "time"
         "net/http"
 
+        routes "server/routes" 
         "github.com/gin-gonic/gin"
         "github.com/gin-contrib/cors"
 )
@@ -22,20 +23,38 @@ var boulders = []boulder{
 
 func main() {
         router := gin.Default()
-
-
         router.Use(cors.New(cors.Config{
                 AllowOrigins:     []string{"http://localhost:5173"},
-                AllowMethods:     []string{"GET"},
+                AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
                 AllowHeaders:     []string{"Content-Type"},
                 ExposeHeaders:    []string{"Content-Length"},
                 AllowCredentials: true,
                 MaxAge: 12 * time.Hour,
         }))
-        router.GET("/boulders", getBoulders)
+
+        routes.AuthRoutes(router)
+        routes.UserRoutes(router)
+
+
         router.Run("localhost:8080")
 }
 
 func getBoulders(c *gin.Context) {
         c.IndentedJSON(http.StatusOK, boulders)
+}
+
+func postBoulder(c *gin.Context) {
+    c.
+}
+
+func updateBoulder(c *gin.Context) {
+
+}
+
+func deleteBoulder(c *gin.Context) {
+
+}
+
+func  patchBoulder(c *gin.Context) {
+
 }
