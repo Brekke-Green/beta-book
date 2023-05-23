b2505b0a591ee6f0db39da24c00b19ccdd3db277
Brekke-Green <brekke.andrew.green@gmail.com>
2023-05-23 10:32:32 -0400

    Comment out future code

--- server/routes/climbRoutes.go
+++ server/routes/climbRoutes.go
@@ -6,10 +6,10 @@ import (
 
 func climbRoutes() {
     
-        router.GET("/climbs", getBoulders)
-        router.GET("/climbs/:climb", getClimb)
-        router.POST("/climbs/:climb", postClimb)
-        router.PUT("/climbs/:climb", updateClimb)
-        router.PATCH("/climbs/:climb", patchClimb)
-        router.DELETE("/climbs/:climb", deleteClimb)
+//        router.GET("/climbs", getBoulders)
+//        router.GET("/climbs/:climb", getClimb)
+//        router.POST("/climbs/:climb", postClimb)
+//        router.PUT("/climbs/:climb", updateClimb)
+//        router.PATCH("/climbs/:climb", patchClimb)
+//        router.DELETE("/climbs/:climb", deleteClimb)
     } 
