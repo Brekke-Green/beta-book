59fe1b992b2ef5d112888622de1e480864f0c926
Brekke-Green <brekke.andrew.green@gmail.com>
2023-06-12 20:43:35 -0400

    Build working user structure for CreateUser functionality

--- server/internal/user/user_handler.go
+++ server/internal/user/user_handler.go
@@ -1,6 +1,10 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Handler struct {
     Service
@@ -13,5 +17,17 @@ func NewHandler(s Service) *Handler {
 }
 
 func (h *Handler) CreateUser(c *gin.Context) {
+    var u CreateUserReq
+    if err := c.ShouldBindJSON(&u); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
 
+    res, err := h.Service.CreateUser(c.Request.Context(), &u)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, res)
 }
